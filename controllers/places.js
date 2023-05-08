7624bb5d8f436caba5e79a131e427a30e3a9c2b7
VincentS972 <vincentsechi@gmail.com>
2023-05-08 13:48:41 -0400

    added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -32,6 +32,19 @@ router.delete('/:id',(req, res) => {
   }
 })
 
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render ('places/edit', { place: places[id]})
+  }
+})
+
 router.post('/', (req, res) => {
   if (!req.body.pic) {
     // Default image if one is not provided
